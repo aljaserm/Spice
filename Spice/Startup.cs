d0efb4515d13023512cfeb94834e94ad7c8d76f8
Mohammed alJaser <aljaserm7@gmail.com>
2019-01-19 10:25:43 -0500

    Set up the project

--- Spice/Startup.cs
+++ Spice/Startup.cs
@@ -66,12 +66,22 @@ namespace Spice
 
             app.UseAuthentication();
 
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "areas",
+                    template: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+            });
+
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+
         }
     }
 }
