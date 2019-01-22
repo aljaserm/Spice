1dbaa98acbd0951b50fe5c47016ca355ee34e160
Mohammed alJaser <aljaserm7@gmail.com>
2019-01-22 18:32:50 -0500

    Completed the work on Category

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -18,6 +18,98 @@ namespace Spice.Areas.Admin.Controllers
             _db = db;
         }
 
+        //get Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        //get Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        ////post Delete
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> DeleteConfirm(int? id)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _db.Update(value);
+        //        await _db.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(value);
+        //}
+
+        //post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Category value)
+        {
+            if (ModelState.IsValid)
+            {
+               var removedCategory= _db.Category.Find(value.Id);
+                if (removedCategory == null)
+                {
+                    return NotFound();
+                }
+                _db.Remove(removedCategory);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(value);
+        }
+
+        //get Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var category = await _db.Category.FindAsync(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        //post Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category value)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(value);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(value);
+        }
+
         //get Create
         public IActionResult Create()
         {
--- Spice/Models/Category.cs
+++ Spice/Models/Category.cs
@@ -9,6 +9,7 @@ namespace Spice.Models
     public class Category
     {
         public int Id { get; set; }
+        [Display(Name ="Category Name")]
         [Required]
         public string Name { get; set; }
     }
