49645e7e7a3678063df53cc057bf7159669b83d4
Mohammed alJaser <aljaserm7@gmail.com>
2019-01-23 20:08:19 -0500

    Completed Sub Category. Started on Menu

---
+++ Spice/Areas/Admin/Controllers/MenuController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using Spice.ViewModels;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _he;
+        [BindProperty]
+        public MenuVM vM { get; set; }
+        public MenuController(ApplicationDbContext db, IHostingEnvironment he)
+        {
+            _db = db;
+            _he = he;
+            vM = new MenuVM()
+            {
+                lstCategory = _db.Category,
+                Menu = new Models.Menu()
+            };
+        }
+
+        //get Create
+        public IActionResult Create()
+        {
+            return View(vM);
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var menu = await _db.Menu.Include(m=>m.Category).Include(m=>m.SubCategory).ToListAsync();
+            return View(menu);
+        }
+    }
+}
--- Spice/Areas/Admin/Controllers/SubCategoryController.cs
+++ Spice/Areas/Admin/Controllers/SubCategoryController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Spice.Data;
+using Spice.Models;
 using Spice.ViewModels;
 
 namespace Spice.Areas.Admin.Controllers
@@ -13,10 +15,115 @@ namespace Spice.Areas.Admin.Controllers
     public class SubCategoryController : Controller
     {
         private ApplicationDbContext _db;
+        [TempData]
+        public string msg { get; set; }
         public SubCategoryController(ApplicationDbContext db)
         {
             _db = db;
         }
+
+        // get Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var subCategory = await _db.SubCategory.Include(s => s.Category).SingleOrDefaultAsync(m => m.Id == id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subCategory);
+        }
+
+        //get Delete
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var subCategory = await _db.SubCategory.Include(s => s.Category).SingleOrDefaultAsync(m => m.Id == id);
+            if (subCategory == null)
+            {
+                return NotFound();
+            }
+
+            return View(subCategory);
+        }
+
+
+        //post Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(SubCategory value)
+        {
+            var subCategory = await _db.SubCategory.SingleOrDefaultAsync(m => m.Id == value.Id);
+            _db.SubCategory.Remove(subCategory);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        // get Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var subs = await _db.SubCategory.SingleOrDefaultAsync(x => x.Id == id);
+
+            if(subs== null)
+            {
+                return NotFound();
+            }
+                
+            CategoryAndSubsVM categoryAndSubsVM = new CategoryAndSubsVM()
+            {
+                lstCategory = await _db.Category.ToListAsync(),
+                SubCategory = subs,
+                lstSubCategory = await _db.SubCategory.OrderBy(s => s.Name).Select(s => s.Name).Distinct().ToListAsync()
+            };
+
+            return View(categoryAndSubsVM);
+        }
+
+        //post edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(CategoryAndSubsVM vM)
+        {
+            if (ModelState.IsValid)
+            {
+                var sub = _db.SubCategory.Include(s => s.Category).Where(s => s.Name == vM.SubCategory.Name && s.Category.Id == vM.SubCategory.CategoryId);
+                if (sub.Count() > 0)
+                {
+                    //error
+                    msg = "Error: The Sub Category" + vM.SubCategory.Name + " already exists in " + sub.First().Category.Name + " Category. Please try another name";
+                }
+                else
+                {
+                    var subcategory= await _db.SubCategory.FindAsync(vM.SubCategory.Id);
+                    subcategory.Name = vM.SubCategory.Name;
+                    await _db.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            CategoryAndSubsVM subsVM = new CategoryAndSubsVM()
+            {
+                lstCategory = await _db.Category.ToListAsync(),
+                SubCategory = vM.SubCategory,
+                lstSubCategory = await _db.SubCategory.OrderBy(s => s.Name).Select(s => s.Name).Distinct().ToListAsync(),
+                StatusMsg = msg
+            };
+            return View(subsVM);
+        }
+
+
 
         //get Create
         public async Task<IActionResult> Create()
@@ -42,21 +149,34 @@ namespace Spice.Areas.Admin.Controllers
                 if (sub.Count() > 0)
                 {
                     //error
+                    msg = "Error: The Sub Category" + vM.SubCategory.Name +" already exists in " + sub.First().Category.Name + " Category. Please try another name"; 
                 }
                 else
                 {
                     _db.SubCategory.Add(vM.SubCategory);
                     await _db.SaveChangesAsync();
-                    RedirectToAction(nameof(Index));
+                    return RedirectToAction(nameof(Index));
                 }
             }
             CategoryAndSubsVM subsVM = new CategoryAndSubsVM()
             {
                 lstCategory = await _db.Category.ToListAsync(),
                 SubCategory = vM.SubCategory,
-                lstSubCategory = await _db.SubCategory.OrderBy(s => s.Name).Select(s => s.Name).Distinct().ToListAsync()
+                lstSubCategory = await _db.SubCategory.OrderBy(s => s.Name).Select(s => s.Name).Distinct().ToListAsync(),
+                StatusMsg=msg
+
             };
             return View(subsVM);
+        }
+
+        [ActionName("GetSubsCategory")]
+        public async Task<IActionResult> GetSubsCategory(int id)
+        {
+            List<SubCategory> subCategories = new List<SubCategory>();
+            subCategories = await (from sc in _db.SubCategory
+                             where sc.CategoryId == id
+                             select sc).ToListAsync();
+            return Json(new SelectList(subCategories,"Id","Name"));
         }
 
 
